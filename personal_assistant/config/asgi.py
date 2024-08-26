7237eb4d72f3c7981dd88094e712d889109e9c5a
denys <denys.shtoma@knu.ua>
2024-08-26 13:18:24 +0300

    complete_reintegration_of_the_project

--- personal_assistant/config/asgi.py
+++ personal_assistant/config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for personal_assistant project.
+ASGI config for config project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -12,6 +12,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_assistant.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_asgi_application()
--- personal_assistant/contacts/admin.py
+++ personal_assistant/contacts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Contact
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone_number' )
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number')
+    fieldsets = (
+        (None, {
+            'fields': ('first_name', 'last_name', 'email', 'phone_number')
+        }),
+        ('Additional Information', {
+            'fields': ('address', 'birthday'),
+            'classes': ('collapse',)
+        }),
+    )
