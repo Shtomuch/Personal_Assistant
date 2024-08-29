35fbdc326454e1e3cdded052357a650ffb4ca175
denys <denys.shtoma@knu.ua>
2024-08-29 15:52:19 +0300

    fixing_bug

--- personal_assistant/contacts/migrations/0003_contact_user.py
+++ personal_assistant/contacts/migrations/0003_contact_user.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("contacts", "0001_initial"),
+        ("contacts", "0002_contact_birthday"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
--- personal_assistant/users/models.py
+++ personal_assistant/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 from django.utils import timezone
 from PIL import Image
