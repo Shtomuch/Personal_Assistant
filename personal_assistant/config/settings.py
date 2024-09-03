21eb79508a6d72ebf9a86a1078800685c553d970
Denys <denys.shtoma@knu.ua>
2024-09-03 10:23:24 +0300

    final_1

--- personal_assistant/config/settings.py
+++ personal_assistant/config/settings.py
@@ -148,7 +148,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = (
     "django.core.mail.backends.smtp.EmailBackend"  # для відправки reset_password
 )
-EMAIL_HOST = env("EMAIL_HOST", default="smtp.meta.com")
+EMAIL_HOST = env("EMAIL_HOST" , default="smtp.meta.com")
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_STARTTLS = False
 EMAIL_USE_SSL = True
@@ -159,44 +159,39 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 AUTH_USER_MODEL = "users.CustomUser"  # щоб джанго бачив перевизначені моделі юзера
 
-USE_SPACES = os.getenv("USE_SPACES") == "TRUE"
+USE_SPACES = os.getenv('USE_SPACES') == 'TRUE'
 
 if USE_SPACES:
     # settings
-    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-    AWS_DEFAULT_ACL = "public-read"
+    AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_ENDPOINT_URL = env("AWS_S3_ENDPOINT_URL")
-    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # static settings
-    AWS_LOCATION = "static"
-    # STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_LOCATION = 'static'
+    #STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # public media settings
-    PUBLIC_MEDIA_LOCATION = "media"
-    MEDIA_URL = f"https://{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/"
-    DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
+    # DEFAULT_FILE_STORAGE = 'hello_django.storage_backends.PublicMediaStorage'
     # private media settings
-    PRIVATE_MEDIA_LOCATION = "private"
-    PRIVATE_FILE_STORAGE = "hello_django.storage_backends.PrivateMediaStorage"
+    PRIVATE_MEDIA_LOCATION = 'private'
+    PRIVATE_FILE_STORAGE = 'hello_django.storage_backends.PrivateMediaStorage'
 else:
-    # STATIC_URL = '/static/'
-    # STATIC_ROOT = BASE_DIR / 'staticfiles'
-    MEDIA_URL = "/media/"
-    MEDIA_ROOT = BASE_DIR / "media"
 
-STATICFILES_DIRS = (BASE_DIR / "static",)
+    # Локальні налаштування для статичних файлів і медіа файлів
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Шлях до статичної папки
-STATIC_URL = "/static/"
+    # Використовуємо локальну файлову систему для медіа файлів
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
-## Вказуємо, що статичні файли знаходяться у папці 'static' у корені проекту
-# STATICFILES_DIRS = [BASE_DIR / 'static']
+    # Налаштування для зберігання файлів аватарів локально
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
-# Опція для збирання статичних файлів у конкретну папку (зазвичай використовується на продакшені)
-STATIC_ROOT = BASE_DIR / "staticfiles"
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- personal_assistant/contacts/urls.py
+++ personal_assistant/contacts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.contact_list, name="contact_list"),
-    # path('search/', views.contact_search, name='contact_search'),
-    path("create/", views.contact_create, name="contact_create"),
-    path("edit/<int:pk>/", views.contact_edit, name="contact_edit"),
-    path("delete/<int:pk>/", views.contact_delete, name="contact_delete"),
+    path('', views.contact_list, name='contact_list'),
+    #path('search/', views.contact_search, name='contact_search'),
+    path('create/', views.contact_create, name='contact_create'),
+    path('edit/<int:pk>/', views.contact_edit, name='contact_edit'),
+    path('delete/<int:pk>/', views.contact_delete, name='contact_delete'),
 ]
