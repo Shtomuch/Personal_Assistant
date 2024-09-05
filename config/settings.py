8a2d81e590854fd17bebbccb4f2efa5b0e1c84fe
Denys <denys.shtoma@knu.ua>
2024-09-05 12:23:38 +0300

    fixing_static_url

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_URL = 'static/'
+STATIC_ROOT = Path(BASE_DIR) / 'static'
 
+STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
